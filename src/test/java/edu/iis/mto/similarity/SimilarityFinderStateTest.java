0c0390ffe3259c6dd54d83de25a48e790bea29dc
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-03-27 14:19:27 +0100

    Stworzenie klasy do testow stanu.

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderStateTest {
+
+    SimilarityFinder similarityFinder;
+    final int[] emptySeq = {};
+    final int[] oneElementSeq = {3};
+    final int[] fiveElementsSeq = {1,2,3,4,5};
+
+    @BeforeEach
+    public void setup() {
+        //MUST BE DONE
+        //similarityFinder = new SimilarityFinder();
+    }
+
+}
