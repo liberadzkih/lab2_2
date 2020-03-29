44066c13978441445bfd3ee868e489b08d5c2ff3
Hubert Salamaga <215871@edu.p.lodz.pl>
2020-03-29 17:51:59 +0200

    add test for Jaccard Similarity on equal collections

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -25,4 +25,16 @@ public class SimilarityFinderTest {
         int[] collection2 = {4,5,6};
         Assert.assertThat(similarityFinder.calculateJaccardSimilarity(collection1, collection2), is(equalTo(0d)));
     }
+
+    @Test
+    public void jaccardSimilarityOnEqualCollections() {
+        similarityFinder = new SimilarityFinder((key, seq) -> {
+            if (key == seq[0] || key == seq[1] || key == seq[2] || key == seq[3])
+                return SearchResult.builder().withFound(true).build();
+            return SearchResult.builder().withFound(false).build();
+        });
+        int[] collection1 = {1,2,3};
+        int[] collection2 = {3,2,1};
+        Assert.assertThat(similarityFinder.calculateJaccardSimilarity(collection1, collection2), is(equalTo(1d)));
+    }
 }
