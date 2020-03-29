3863e04d6050e575c7437cdf353c2388a8de270d
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-03-30 01:28:57 +0200

    Changed mock to handle history of invocation

--- src/test/java/edu/iis/mto/similarity/SequenceSearcherMock.java
+++ src/test/java/edu/iis/mto/similarity/SequenceSearcherMock.java
@@ -1,9 +1,25 @@
 package edu.iis.mto.similarity;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherMock implements SequenceSearcher {
+    private Set<Invocation> invocationMemory = new HashSet<>();
+
+    public int getNumberOfInvocations() {
+        return invocationMemory.size();
+    }
+
+    public boolean checkIfContainsInvocation(int elem, int[] seq) {
+        if (!invocationMemory.contains(new Invocation(elem, seq))) {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public SearchResult search(int elem, int[] seq) {
@@ -20,6 +36,43 @@ public class SequenceSearcherMock implements SequenceSearcher {
             }
             position++;
         }
+        invocationMemory.add(new Invocation(elem, seq));
         return builder.build();
+    }
+
+    private static class Invocation {
+
+        int elem;
+        int[] seq;
+
+        Invocation(int elem, int[] seq) {
+            this.elem = elem;
+            this.seq = seq;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + elem;
+            result = prime * result + Arrays.hashCode(seq);
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Invocation other = (Invocation) obj;
+            if (elem != other.elem)
+                return false;
+            if (!Arrays.equals(seq, other.seq))
+                return false;
+            return true;
+        }
     }
 }
