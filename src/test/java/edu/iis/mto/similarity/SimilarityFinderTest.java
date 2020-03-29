c3608dc9b1606bc54050223dbb52d73ee8ab5b37
pawelek103 <215675@edu.p.lodz.pl>
2020-03-29 19:00:07 +0200

    Added test for JaccardSimilarity with the same sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -3,6 +3,9 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
@@ -14,6 +17,22 @@ class SimilarityFinderTest {
         int[] sequence1 = { };
         int[] sequence2 = { };
         simFinder = new SimilarityFinder((key, seq) ->  SearchResult.builder().build());
+        Assert.assertThat(simFinder.calculateJackardSimilarity(sequence1, sequence2), is(equalTo(1.0d)));
+    }
+
+    @Test
+    public void findInTheSameSequences() {
+        int[] sequence1 = { 0, 1, 2, 3, 4, 5 };
+        int[] sequence2 = { 3, 0, 5, 2, 1, 4 };
+
+        simFinder = new SimilarityFinder((key, seq) -> {
+            if (IntStream.of(seq).anyMatch(x -> x == key)) {
+                return SearchResult.builder().withFound(true).build();
+            } else {
+                return SearchResult.builder().withFound(false).build();
+            }
+        });
+
         Assert.assertThat(simFinder.calculateJackardSimilarity(sequence1, sequence2), is(equalTo(1.0d)));
     }
 }
