26b4e72db07ad9d35e0e9c6a4d80bb8176646b64
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-03-29 17:01:09 +0200

    2_2 pkt 3b testy zachowania zrobiony grunt pod testy zachowania
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/main/java/edu/iis/mto/search/SequenceSearcherDubler.java
+++ src/main/java/edu/iis/mto/search/SequenceSearcherDubler.java
@@ -1,16 +1,44 @@
 package edu.iis.mto.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SequenceSearcherDubler implements SequenceSearcher {
+
+    @Override
+    public SearchResult search(int elem, int[] seq) {
+        return null;
+    }
 
     private class BehaviuorInformation {
         int elem;
         int[] seq;
 
+        public BehaviuorInformation(int elem, int[] seq) {
+            this.elem = elem;
+            this.seq = seq;
+        }
 
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
     }
 
-    @Override
-    public SearchResult search(int elem, int[] seq) {
-        return null;
+    private List<BehaviuorInformation> behaviuorInformationList = new ArrayList<>();
+
+    public int getSearchInvocationAmout() {
+        return behaviuorInformationList.size();
+    }
+
+    public void verifySearchInvocation(int elem, int [] seq){
+        if(behaviuorInformationList.contains(new BehaviuorInformation(elem,seq))){
+            throw  new AssertionError("Expected method execution didn't happen or was not registered!");
+        }
     }
 }
