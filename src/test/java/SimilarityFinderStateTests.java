2d066d6fe4ba06a9f5b84a3298f42a8f14ef90b7
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-29 18:56:38 +0200

    Refactored and added new test cases to SimilarFinderStateTests.

--- src/test/java/SimilarityFinderStateTests.java
+++ src/test/java/SimilarityFinderStateTests.java
@@ -7,10 +7,19 @@ import org.junit.jupiter.api.Test;
 public class SimilarityFinderStateTests {
 
     int[] seq1 = {0, 1, 2, 3, 4};
-    int[] seq2 = {5, 6, 7, 8, 9};
+    int[] seq2 = {3, 4, 5, 6, 7};
+    int[] emptySeq = {};
+
+    SequenceSearcher standardSequenceSearcher = (elem, seq) -> {
+        for (int i : seq) {
+            if (i == elem)
+                return SearchResult.builder().withFound(true).build();
+        }
+        return SearchResult.builder().withFound(false).build();
+    };
 
     @Test
-    public void zeroJaccardIndexTest(){
+    public void twoDifferentSequencesTest() {
         SequenceSearcher stub = (elem, seq) -> SearchResult.builder().withFound(false).build();
 
         SimilarityFinder similarityFinder = new SimilarityFinder(stub);
@@ -20,12 +29,36 @@ public class SimilarityFinderStateTests {
     }
 
     @Test
-    public void oneJaccardIndexTest(){
+    public void twoSameSequencesTest() {
         SequenceSearcher stub = (elem, seq) -> SearchResult.builder().withFound(true).build();
 
         SimilarityFinder similarityFinder = new SimilarityFinder(stub);
         double jackardSimilarity = similarityFinder.calculateJackardSimilarity(seq1, seq1);
 
         Assertions.assertEquals(1.0d, jackardSimilarity);
+    }
+
+    @Test
+    public void regularTest() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(standardSequenceSearcher);
+        double jackardSimilarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertEquals(0.25d, jackardSimilarity);
+    }
+
+    @Test
+    public void emptySequenceTest1() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(standardSequenceSearcher);
+        double jackardSimilarity = similarityFinder.calculateJackardSimilarity(emptySeq, seq1);
+
+        Assertions.assertEquals(0.0d, jackardSimilarity);
+    }
+
+    @Test
+    public void emptySequenceTest2() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(standardSequenceSearcher);
+        double jackardSimilarity = similarityFinder.calculateJackardSimilarity(seq1, emptySeq);
+
+        Assertions.assertEquals(0.0d, jackardSimilarity);
     }
 }
