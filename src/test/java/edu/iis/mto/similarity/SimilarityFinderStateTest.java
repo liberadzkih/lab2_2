61ef41ee11085e7277c5d2add36ccb30692747ea
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-03-29 17:53:59 +0200

    Poprawiono testy stanu
    Signed-off-by: Bilant.Andrzej <215679@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -15,32 +15,32 @@ class SimilarityFinderStateTest {
 
     @Test
     void calculateJackardSimilarity_bothSequencesHaveElements_thereAreSameElements() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(seq1, seq2), 0.2, 0.01);
+        assertEquals(0.2, similarityFinder.calculateJackardSimilarity(seq1, seq2), 0.01);
     }
 
     @Test
     void calculateJackardSimilarity_firstSequenceHasElements_secondSequenceIsEmpty() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(seq1, empty), 0, 0.01);
+        assertEquals(0, similarityFinder.calculateJackardSimilarity(seq1, empty), 0.01);
     }
 
     @Test
     void calculateJackardSimilarity_firstSequenceIsEmpty_secondSequenceHasElements() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(empty, seq2), 0, 0.01);
+        assertEquals(0, similarityFinder.calculateJackardSimilarity(empty, seq2), 0.01);
     }
 
     @Test
     void calculateJackardSimilarity_bothSequencesAreEmpty() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(empty, empty), 1, 0.01);
+        assertEquals(1, similarityFinder.calculateJackardSimilarity(empty, empty), 0.01);
     }
 
     @Test
     void calculateJackardSimilarity_bothSequencesHaveElements_noMatchingElements() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(seq2, seq3), 0, 0.01);
+        assertEquals(1, similarityFinder.calculateJackardSimilarity(seq2, seq3), 0.01);
     }
 
     @Test
     void calculateJackardSimilarity_sequencesAreTheSame() {
-        assertEquals(similarityFinder.calculateJackardSimilarity(seq2, seq2), 1, 0.01);
+        assertEquals(1, similarityFinder.calculateJackardSimilarity(seq2, seq2), 0.01);
     }
 
 }
