d1498254db3e5b786431d1bbf11d51d042a7a123
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-03-29 16:17:27 +0200

    2_2 pkt 3a testy stanu dodany test seq1 jest pusta
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -0,0 +1,30 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearcherDubler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderStateTest {
+    int[] testseqEmpty = {};
+    int[] testseqOneElement = {1};
+    int[] testseqTenElements = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    SimilarityFinder similarityFinder;
+
+    @BeforeEach
+    void setUp() {
+        //similarityFinder = new SimilarityFinder();
+    }
+
+    @Test
+    void calculateJackardSimilarity_bothSeqAreEmpty() {
+        assertEquals(1.0d, similarityFinder.calculateJackardSimilarity(testseqEmpty, testseqEmpty));
+    }
+
+    @Test
+    void calculateJackardSimilarity_seq1IsEmpty() {
+        assertEquals(0.0d,similarityFinder.calculateJackardSimilarity(testseqEmpty,testseqTenElements));
+    }
+
+}
