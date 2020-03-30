a1663b2b1b79ab2fa290496ffde7ab773c71ee3d
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-03-30 17:42:53 +0200

    fourth state test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,6 +11,8 @@ class SimilarityFinderTest {
     int[] seq3={2,3,5,6};
     int[] seq4={9,7,4,1};
     int[] seq5={3};
+    int[] seq6={4,5,6};
+    int[]seq7={5,6,4};
     SimilarityFinder similarityFinder = new SimilarityFinder(new MockForSequenceSearcher());
 
     @Test void return_zero_if_two_single_element_sequences_dont_have_the_same_element() {
@@ -24,4 +26,9 @@ class SimilarityFinderTest {
     @Test void  return_zero_if_two_multi_elements_sequences_dont_have_the_same_element() {
         assertEquals(0.0,similarityFinder.calculateJackardSimilarity(seq3,seq4));
     }
+
+    @Test void  return_one_if_two_multi_elements_sequences_have_the_same_elements() {
+        assertEquals(1.0,similarityFinder.calculateJackardSimilarity(seq6,seq7));
+    }
+
 }
