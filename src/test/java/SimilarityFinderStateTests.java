dea4e824569fe4581ac87761f6c82f2f5ef97b4f
Lisek8 <fafik989@gmail.com>
2020-03-31 00:32:48 +0200

    Null sequences test added

--- src/test/java/SimilarityFinderStateTests.java
+++ src/test/java/SimilarityFinderStateTests.java
@@ -3,13 +3,20 @@ import edu.iis.mto.similarity.SimilarityFinder;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimilarityFinderStateTests {
     private int[] emptySequence = {};
-    
+
     @Test
     public void emptySequencesTest() {
         SimilarityFinder similarityFinder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
         assertEquals(1, similarityFinder.calculateJackardSimilarity(emptySequence, emptySequence), 0.001);
+    }
+
+    @Test
+    public void nullSequencesTest() {
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
+        assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
     }
 }
