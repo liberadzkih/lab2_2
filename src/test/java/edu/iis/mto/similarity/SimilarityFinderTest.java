ed43d1c4622fdc469b0544613c18c5019e624c81
MagdalenaDrozdz <215707@edu.p.lodz.pl>
2020-03-30 15:10:21 +0200

    zrobione 2

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -29,27 +29,6 @@ class SimilarityFinderTest {
         assertEquals(0,result);
     }
 
-    @Test
-    void two_array_of_two_the_same_element_it_should_evaluate_to_half(){
-        int[] sampleArray={1,2,5,6};
-        int[] sampleArray2={3,4,5,6};
-        SimilarityFinder similarityFinder=new SimilarityFinder(new SequenceSearcherImplementation());
-
-        double result = similarityFinder.calculateJackardSimilarity(sampleArray,sampleArray2);
-
-        assertEquals(0.5,result);
-    }
-
-    @Test
-    void two_array_of_one_the_same_element_it_should_evaluate_to_quarter(){
-        int[] sampleArray={1,2,5,6};
-        int[] sampleArray2={3,4,5,7};
-        SimilarityFinder similarityFinder=new SimilarityFinder(new SequenceSearcherImplementation());
-
-        double result = similarityFinder.calculateJackardSimilarity(sampleArray,sampleArray2);
-
-        assertEquals(0.25,result);
-    }
 
     @Test
     void two_array_with_one_empty_should_evaluate_to_zero(){
