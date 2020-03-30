ae64b7d830d7859bf3440fff47e1631682e6b6bd
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-03-30 10:35:28 +0200

    poprawione testy stanu
    Signed-off-by: Kowalczyk.Bartosz <21577@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,41 +13,57 @@ class SimilarityFinderStateTest {
     int[] testseqDifferentElement = {41};
     int[] testseqTenElements = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     SimilarityFinder similarityFinder;
+    SequenceSearcher stub;
 
-    @BeforeEach
-    void setUp() {
-        //similarityFinder = new SimilarityFinder();
-    }
+
 
     @Test
     void calculateJackardSimilarity_bothSeqAreEmpty() {
+        stub = (elem, seq) -> SearchResult.builder().withFound(false).build();
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(1.0d, similarityFinder.calculateJackardSimilarity(testseqEmpty, testseqEmpty));
     }
 
     @Test
     void calculateJackardSimilarity_seq1IsEmpty() {
+        stub = (elem, seq) -> SearchResult.builder().withFound(false).build();
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(0.0d, similarityFinder.calculateJackardSimilarity(testseqEmpty, testseqTenElements));
     }
 
     @Test
     void calculateJackardSimilarity_seq2IsEmpty() {
+        stub = (elem, seq) -> SearchResult.builder().withFound(false).build();
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(0.0d, similarityFinder.calculateJackardSimilarity(testseqTenElements, testseqEmpty));
     }
 
     @Test
     void calculateJackardSimilarity_oneElementMatches() {
+        stub = (elem, seq) -> {
+            for (int i : seq) {
+                if (i == elem)
+                    return SearchResult.builder().withFound(true).build();
+            }
+            return SearchResult.builder().withFound(false).build();
+        };
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(0.1d, similarityFinder.calculateJackardSimilarity(testseqOneElement, testseqTenElements));
         assertEquals(0.1d, similarityFinder.calculateJackardSimilarity(testseqTenElements, testseqOneElement));
     }
 
     @Test
     void calculateJackardSimilarity_allElementsMatch() {
+        stub = (elem, seq) -> SearchResult.builder().withFound(true).build();
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(1.0d, similarityFinder.calculateJackardSimilarity(testseqOneElement, testseqOneElement));
         assertEquals(1.0d, similarityFinder.calculateJackardSimilarity(testseqTenElements, testseqTenElements));
     }
 
     @Test
     void calculateJackardSimilarity_noMatchingElements() {
+        stub = (elem, seq) -> SearchResult.builder().withFound(false).build();
+        similarityFinder = new SimilarityFinder(stub);
         assertEquals(0.0d,similarityFinder.calculateJackardSimilarity(testseqOneElement,testseqDifferentElement));
         assertEquals(0.0d,similarityFinder.calculateJackardSimilarity(testseqDifferentElement,testseqOneElement));
         assertEquals(0.0d,similarityFinder.calculateJackardSimilarity(testseqTenElements,testseqDifferentElement));
