0f7825e3fdfb3ec3543dea5d491aa7e7679a574a
Lis-Marciniak.Jędrzej <215799@edu.p.lodz.pl>
2020-03-31 00:37:37 +0200

    One empty sequence test added

--- src/test/java/SimilarityFinderStateTests.java
+++ src/test/java/SimilarityFinderStateTests.java
@@ -7,16 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimilarityFinderStateTests {
     private int[] emptySequence = {};
+    private int[] regularSequence = {-2, -1, 0, 1, 2};
 
     @Test
     public void emptySequencesTest() {
         SimilarityFinder similarityFinder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
-        assertEquals(1, similarityFinder.calculateJackardSimilarity(emptySequence, emptySequence), 0.001);
+        assertEquals(1, similarityFinder.calculateJackardSimilarity(emptySequence, emptySequence));
     }
 
     @Test
     public void nullSequencesTest() {
         SimilarityFinder similarityFinder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
         assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
+    }
+
+    @Test
+    public void emptySequenceTest() {
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
+        assertEquals(0, similarityFinder.calculateJackardSimilarity(regularSequence, emptySequence));
     }
 }
