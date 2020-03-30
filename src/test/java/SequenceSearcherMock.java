337960049ee0b67cab51458b162c27d6e342047d
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-30 22:48:56 +0200

    Add invocations check in SequenceSearcherMock

--- src/test/java/SequenceSearcherMock.java
+++ src/test/java/SequenceSearcherMock.java
@@ -1,10 +1,36 @@
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class SequenceSearcherMock implements SequenceSearcher {
+
+    private Set<Invocation> invocationSet = new HashSet<>();
+
     @Override
     public SearchResult search(int elem, int[] seq) {
+        invocationSet.add(new Invocation(elem, seq));
         return SearchResult.builder().build();
+    }
+
+    void verifyInvocationNumber(int invocationNumber){
+        if(invocationNumber != invocationSet.size())
+            throw new AssertionError("Bad number of invocations.");
+    }
+    void verifyInvocation(int elem, int[] seq) {
+        if (!invocationSet.contains(new Invocation(elem, seq)))
+            throw new AssertionError("Invocation didn't match.");
+    }
+
+    private static class Invocation {
+
+        int elem;
+        int[] seq;
+
+        Invocation(int elem, int[] seq) {
+            this.elem = elem;
+            this.seq = seq;
+        }
     }
 }
