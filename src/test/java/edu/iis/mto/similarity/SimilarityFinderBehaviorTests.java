0232df6816e12ccbf783f784ea31eb0937da25b7
frąg.przemysław <215719@edu.p.lodz.pl>
2020-04-01 13:55:20 +0200

    Fixed issue with Travis

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
@@ -1,33 +1,31 @@
 package edu.iis.mto.similarity;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 import edu.iis.mto.search.SimpleSearcher;
 
 public class SimilarityFinderBehaviorTests {
-	private static final int[] EMPTY_SEQUENCE = { };
 	private static final int[] SEQUENCE = { 1, 2, 3, 4 };
 	
 	private static SimilarityFinder similarityFinder = new SimilarityFinder(new SimpleSearcher());
 	
-	@Test(expected = NullPointerException.class)
+	@Test
 	public void nullAsSeqencesThrows() {
-		similarityFinder.calculateJackardSimilarity(null, null);
+		assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
 	}
 	
-	@Test(expected = NullPointerException.class)
 	public void nullAsFirstSeqencesThrows() {
-		similarityFinder.calculateJackardSimilarity(null, SEQUENCE);
+		assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, SEQUENCE));
 	}
 	
-	@Test(expected = NullPointerException.class)
 	public void nullAsSecondSeqencesThrows() {
-		similarityFinder.calculateJackardSimilarity(SEQUENCE, null);
+		assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(SEQUENCE, null));
 	}
 	
-	@Test(expected = NullPointerException.class)
 	public void nullAsFinderThrows() {
 		similarityFinder = new SimilarityFinder(null);
-		similarityFinder.calculateJackardSimilarity(SEQUENCE, SEQUENCE);
+		assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(SEQUENCE, SEQUENCE));
 	}
 }
