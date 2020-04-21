e43cd8d343a8f5b80afb2a3faa66ab92410ea573
AndrzejBilant <andrzej.bilant@gmail.com>
2020-04-21 19:28:28 +0200

    poprawiono testy
    Signed-off-by: AndrzejBilant <andrzej.bilant@gmail.com>

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -48,6 +48,7 @@ class SimilarityFinderBehaviourTest {
     @BeforeEach
     void intit() {
         mockSequenceSearcher = new MockSequenceSearcher();
+        similarityFinder=new SimilarityFinder(mockSequenceSearcher);
     }
 
     @Test
