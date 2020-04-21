e51821904857fd185118bc04a8d55125b5ea00b7
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-04-21 19:16:05 +0200

    poprawki bo nie wiedzialem o co chodzilo z builderem
    Signed-off-by: Kowalczyk.Bartosz <21577@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
+++ src/test/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
@@ -3,15 +3,21 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SequenceSearcherDubler implements SequenceSearcher {
 
+    public SequenceSearcherDubler() {
+        this.behaviuorInformationList = new ArrayList<>();
+    }
+
     @Override
     public SearchResult search(int elem, int[] seq) {
         behaviuorInformationList.add(new BehaviuorInformation(elem, seq));
-        return null;
+        return SearchResult.builder().build();
     }
 
     private static class BehaviuorInformation {
@@ -30,11 +36,14 @@ public class SequenceSearcherDubler implements SequenceSearcher {
 
         @Override
         public boolean equals(Object obj) {
-            return super.equals(obj);
+            if(this == obj) return true;
+            else if (obj == null || getClass() != obj.getClass()) return false;
+            BehaviuorInformation that = (BehaviuorInformation) obj;
+            return elem == that.elem && Arrays.equals(seq,that.seq);
         }
     }
 
-    private List<BehaviuorInformation> behaviuorInformationList = new ArrayList<>();
+    private List<BehaviuorInformation> behaviuorInformationList;
 
     public int getSearchInvocationAmout() {
         return behaviuorInformationList.size();
