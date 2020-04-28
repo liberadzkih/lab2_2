da6c8b4aaf437c2eee23cf3bc888b2b0d7bc6ce1
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-04-28 10:32:19 +0200

    Refactored behavioural tests

--- src/main/java/edu/iis/mto/search/SequenceSearcherMock.java
+++ src/main/java/edu/iis/mto/search/SequenceSearcherMock.java
@@ -5,44 +5,12 @@ import java.util.OptionalInt;
 
 public class SequenceSearcherMock implements SequenceSearcher {
 
-    int elemParam;
-    int[] seqParam;
-
     static int count = 0;
 
     @Override
     public SearchResult search(int elem, int[] seq) {
-//
-//        count++;
-//
-//        elemParam = elem;
-//        seqParam = seq;
-//
-//        SearchResult.Builder builder = SearchResult.builder();
-//
-//        OptionalInt result = Arrays.stream(seq, 0, seq.length).filter(var -> var == elem).findFirst();
-//
-//        builder.withFound(result.isPresent());
-//        if(result.isPresent()) builder.withPosition(result.getAsInt());
-//
-//        return builder.build();
+        count++;
         return SearchResult.builder().withFound(true).withPosition(0).build();
-    }
-
-    public int getElemParam() {
-        return elemParam;
-    }
-
-    public int[] getSeqParam() {
-        return seqParam;
-    }
-
-    public void setElemParam(int elemParam) {
-        this.elemParam = elemParam;
-    }
-
-    public void setSeqParam(int[] seqParam) {
-        this.seqParam = seqParam;
     }
 
     public static int getCount() {
