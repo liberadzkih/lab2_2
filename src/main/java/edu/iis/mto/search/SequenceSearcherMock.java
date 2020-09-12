e27ad4162727c440c0c94e3b914e65988640d4f0
liberadzkih <liberadzkih@gmail.com>
2020-09-12 16:00:11 +0200

    Create SequenceSearcherMock

---
+++ src/main/java/edu/iis/mto/search/SequenceSearcherMock.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.search;
+
+public class SequenceSearcherMock implements SequenceSearcher {
+
+    @Override public SearchResult search(int elem, int[] seq) {
+        if (seq == null || seq.length == 0) {
+            throw new IllegalArgumentException("Sequence is empty");
+        }
+
+        for (int i = 0; i < seq.length; i++) {
+            if (elem == seq[i]) {
+                return SearchResult.builder().withFound(true).withPosition(i).build();
+            }
+        }
+        return SearchResult.builder().withPosition(-1).withFound(false).build();
+    }
+}
