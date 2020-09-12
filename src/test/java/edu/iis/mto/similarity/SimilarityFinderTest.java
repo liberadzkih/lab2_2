b85f350872119e9d8968a0dd892daf6bdd4dd42f
liberadzkih <liberadzkih@gmail.com>
2020-09-12 15:10:48 +0200

    Created empty test cases

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,37 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderTest {
+
+    @Test public void jackardSimilarity_firstSequenceEmpty() {
+    }
+
+    @Test public void jackardSimilarity_secondSequenceEmpty() {
+    }
+
+    @Test public void jackardSimilarity_bothSequencesEmpty() {
+    }
+
+    @Test public void jackardSimilarity_sequencesSameContent() {
+    }
+
+    @Test public void jackardSimilarity_sequencesSameContent_sequencesDifferentOrder() {
+    }
+
+    @Test public void jackardSimilarity_sequencesSameContent_sequencesDifferentLength() {
+    }
+
+    @Test public void jackardSimilarity_sequencesDifferentContent_sequencesSameLength() {
+    }
+
+    @Test public void jackardSimilarity_sequencesDifferentContent_sequencesDifferentLength() {
+    }
+
+    @Test public void jackardSimilarity_almostSameContent_sequencesSameLength() {
+    }
+
+    @Test public void jackardSimilarity_almostSameContent_oneMoreElementInSecondSequence() {
+    }
+
+}
